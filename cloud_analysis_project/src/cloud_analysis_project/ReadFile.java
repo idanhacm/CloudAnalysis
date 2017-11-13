de1d3c0679e3447da29bb6685217cc9e3220e058
idanhacm <32680269+idanhacm@users.noreply.github.com>
2017-11-13 17:58:32 +0200

    Update ReadFile.java

--- cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
+++ cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
@@ -6,26 +6,41 @@ import java.util.Scanner;
 
 public class ReadFile
 {
-	//255 int array, containing the distribution of the pixel colors
-	public int distribution[];
+	//256 int array, containing the distribution of the pixel colors
+	private int distribution[];
+	
 	//File reader object
 	private Scanner file_reader;
+	
 	//The constant number range given
-	final int NUMBER_RANGE = 256;
+	final private int NUMBER_RANGE = 256;
 	
 	//Default c'tor 
 	public ReadFile()
 	{
 		distribution = new int[NUMBER_RANGE];
 	}
+	
 	//string c'tor, read from file_name
 	public ReadFile(String file_name) 
 	{
 		distribution = new int[NUMBER_RANGE];
-		read_file(file_name);
+		file_to_dis(file_name);
+	}
+	//get average and calculate
+	public double get_mean()
+	{
+		double sum=0;
+		double count=0;
+		for(int i=0; i<NUMBER_RANGE; i++)
+		{
+			count += distribution[i];
+			sum += distribution[i]*i;
+		}
+		return sum/count;
 	}
 	//File reader function
-	public void read_file(String file_name)
+	public void file_to_dis(String file_name)
 	{
 		try 
 		{
@@ -48,9 +63,9 @@ public class ReadFile
 			if(file_reader.hasNextInt())
 			{
 				int index = file_reader.nextInt();
-				if(index > 255 || index < 0)
-					index = 255;
-				System.out.println(index);
+				
+				if(index >= NUMBER_RANGE || index < 0)
+					continue;
 				distribution[index]++;
 			}else
 			{
@@ -58,17 +73,11 @@ public class ReadFile
 			}
 		}
 	}
-	//TODO: this function is for programmer use only
-	public void print_arr()
-	{
-		for(int i=0; i<256; i++)
-		{
-			System.out.println(i+": "+ distribution[i]);
-		}
-	}
+	
 	//TODO: useless main for debugging purposes only
-	public static void main(String[] args) {
-		ReadFile rf = new ReadFile("IR.txt");
-		rf.print_arr();
+	public static void main(String[] args) 
+	{
+		ReadFile rf = new ReadFile("n.txt");
+		System.out.println(rf.get_mean());
 	}
 }
