002b11d0750797d9005fb6290b511fe9a65a53c0
idanhacm <32680269+idanhacm@users.noreply.github.com>
2017-11-13 14:56:02 +0200

    Update ReadFile.java

--- cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
+++ cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
@@ -7,16 +7,25 @@ import java.util.Scanner;
 
 public class ReadFile
 {
+	//255 int array, containing the distribution of the pixel colors
 	public int distribution[];
+	//File reader object
 	private Scanner file_reader;
+	//The constant number range given
 	final int NUMBER_RANGE = 256;
 	
+	//Default c'tor 
+	public ReadFile()
+	{
+		distribution = new int[NUMBER_RANGE];
+	}
+	//string c'tor, read from file_name
 	public ReadFile(String file_name) 
 	{
 		distribution = new int[NUMBER_RANGE];
 		read_file(file_name);
 	}
-	
+	//File reader function
 	public void read_file(String file_name)
 	{
 		try 
@@ -50,6 +59,7 @@ public class ReadFile
 			}
 		}
 	}
+	//TODO: this function is for programmer use only
 	public void print_arr()
 	{
 		for(int i=0; i<255; i++)
@@ -57,6 +67,7 @@ public class ReadFile
 			System.out.println(i+": "+ distribution[i]);
 		}
 	}
+	//TODO: useless main for debugging purposes only
 	public static void main(String[] args) {
 		ReadFile rf = new ReadFile("IR.txt");
 		rf.print_arr();
