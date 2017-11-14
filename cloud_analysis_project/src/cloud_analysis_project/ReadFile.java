2db6cbf93dd5eb265f6a4e75540c26ddb2f1b686
idanhacm <32680269+idanhacm@users.noreply.github.com>
2017-11-14 09:25:47 +0200

    Update ReadFile.java

--- cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
+++ cloud_analysis_project/src/cloud_analysis_project/ReadFile.java
@@ -8,46 +8,27 @@ public class ReadFile
 {
 	//256 int array, containing the distribution of the pixel colors
 	private int distribution[];
-	
 	//File reader object
 	private Scanner file_reader;
-	
 	//The constant number range given
 	final private int NUMBER_RANGE = 256;
 	
 	//Default c'tor 
-	public ReadFile()
-	{
-		distribution = new int[NUMBER_RANGE];
+	public ReadFile() {
+		distribution = new int[NUMBER_RANGE]; 
 	}
 	
 	//string c'tor, read from file_name
-	public ReadFile(String file_name) 
-	{
+	public ReadFile(String file_name) {
 		distribution = new int[NUMBER_RANGE];
 		file_to_dis(file_name);
 	}
-	//get average and calculate
-	public double get_mean()
-	{
-		double sum=0;
-		double count=0;
-		for(int i=0; i<NUMBER_RANGE; i++)
-		{
-			count += distribution[i];
-			sum += distribution[i]*i;
-		}
-		return sum/count;
-	}
-	//File reader function
-	public void file_to_dis(String file_name)
-	{
-		try 
-		{
+	//File reader to distribution function 
+	public void file_to_dis(String file_name) {
+		try {
 			file_reader = new Scanner(new File(file_name));
 		}
-		catch (FileNotFoundException e) 
-		{
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
@@ -78,6 +59,9 @@ public class ReadFile
 	public static void main(String[] args) 
 	{
 		ReadFile rf = new ReadFile("n.txt");
-		System.out.println(rf.get_mean());
+		for(int i=0; i<rf.distribution.length; i++)
+		{
+			System.out.println(i+ ": " + rf.distribution[i]);
+		}
 	}
 }
