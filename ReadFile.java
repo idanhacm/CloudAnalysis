8b60d53b499846387ad55ee0d35811edaa5dbea3
idanhacm <32680269+idanhacm@users.noreply.github.com>
2017-11-27 13:57:43 +0200

    Update ReadFile.java
    החלטתי שיהיה הכי יעיל להשתמש במחלקה הזאת כמחלקה סטטית, זאת אומרת שכשאתה רוצה להשתמש בה אתה פשוט עושה כך
    ReadFile.method()....
    בינתיים יש רק פונצקיה אחת אז זה ממש יעיל ונראה טוב ככה
    השימוש הוא כזה
    ReadFile,file_to_dis(int[256], filename)
    המתודה דואגת לפתוח ולסגור את הקובץ וממלאת את המערך שנתת לה בערכים שהיו בקובץ.

--- ReadFile.java
+++ ReadFile.java
@@ -7,34 +7,12 @@ public class ReadFile
 {
 	//The constant number range given
 	final private int NUMBER_RANGE = 256;
-	Scanner file_reader;
 	
-	//Default c'tor 
-	public ReadFile() {
-		try {
-			file_reader = new Scanner(new File("input.dat"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public ReadFile(String file_name) {
-		try {
-			file_reader = new Scanner(new File(file_name));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	public void open_file(String file_name) {
-		try {
-			file_reader = new Scanner(new File(file_name));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
 	//File scanner to distribution function
-	//usage: distribution need to be 256 in length integer, with a legit file scanner
-	public boolean file_to_dis(int[] distribution) {
+	//usage: distribution need to be 256 in length integer, with a legit file name
+	public static boolean file_to_dis(int[] distribution, String file_name) throws FileNotFoundException {
+		Scanner file_reader = new Scanner(new File(file_name));
+		
 		if(distribution.length != 256)
 			return false;
 
